2f8da5ce8dc1565104a0dd11297cb421cda71bf5
FINDINGSOM <50260279+FINDINGSOM@users.noreply.github.com>
2021-01-08 11:42:15 +0530

    Create ReverseTheArray.cpp

---
+++ ReverseTheArray.cpp
@@ -0,0 +1,21 @@
+  
+#include<iostream>
+
+using namespace std;
+int main(){
+int n;
+   int arr[n], i;
+   cout<<"enter the number of elements";
+   cin>>n;
+    cout<<"Enter  Array Elements: ";
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+    cout<<"\nThe Original Array is:\n";
+    for(i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+    cout<<"\n\nThe Reverse of Given Array is:\n";
+    for(i=(n-1); i>=0; i--)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+    return 0;
+}
