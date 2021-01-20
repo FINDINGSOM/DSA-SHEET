7607434926836de6cd41b90a4883216235eb89f2
FINDINGSOM <50260279+FINDINGSOM@users.noreply.github.com>
2021-01-20 22:10:50 +0530

    Create Reversestring.cpp

---
+++ Reversestring.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+char s1[100], c = 'a';
+    int n1, i = 0;
+
+    cout << "\n\nEnter the String you want to check : ";
+    cin >> s1;
+
+    //Computing string length without using system defined method
+    while (c != '\0')
+    {
+        c = s1[i++];
+    }
+
+    n1 = i-1;
+    char s2[n1+1];
+
+    cout << "Length of the entered string is : " << n1 << "\n\n";
+
+    i = 0;
+    //Computing reverse of the String without using system defined method
+    while (i != n1 + 1)
+    {
+        s2[i] = s1[n1 - i - 1];
+        i++;
+    }
+
+    cout << "Reverse of the entered string is : " << s2 << "\n\n\n";
+
+    i = 0;
+    
+    while (i != n1)
+    {
+        if (s2[i] != s1[i])
+            break;
+
+        i++;
+    }
+
+    if (i != n1)
+        cout << "The String \"" << s1 << "\"" << " is not a Palindrome.";
+    else
+        cout << "The String \"" << s1 << "\"" << " is a Palindrome.";
+
+    cout << "\n\n";
+
+    return 0;
+}
